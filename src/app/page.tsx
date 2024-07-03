bf5e1da248bad7d1697a8fa8e0c00da666457364
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2024-07-03 17:44:41 +0530

    added all the item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,104 @@
-import Image from "next/image";
+"use client"
+import { Button } from "@/components/ui/button";
+import CollegeCard from "@/components/ui/CollegeCard";
+import { Input } from "@/components/ui/input";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+
+
+type College = {
+  alpha_two_code: string
+  country: string
+  domains: string[] | null
+  name: string
+  "state-province": string | null
+}
 
 export default function Home() {
+  const [searchText, setSearchText] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [filterData, setFilterData] = useState([]);
+  const [filter, setFilter] = useState('');
+  const [stateProvidance, setStateProvidance] = useState<string[]>([]);
+
+  const handleChange = (e: any) => {
+    setSearchText(e.target.value);
+  }
+
+  const searchCountry = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(`http://universities.hipolabs.com/search?country=${searchText}`)
+      setData(res.data);
+      setFilterData(res.data);
+      const stateData: string[] | null = res.data.map((item: any) => item['state-province']);
+      const uniqueValuesSet = new Set(stateData);
+      setStateProvidance(Array.from(uniqueValuesSet))
+      setFilter('')
+      console.log(stateProvidance)
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false)
+    }
+  }
+  const updateFilter = (e: string) => {
+    setFilter(e);
+    console.log("filter", filter)
+    const fData = data.filter(item => item['state-province'] === filter)
+    setFilterData(fData)
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="flex flex-col items-center justify-center p-5 ">
+        <div className="flex flex-row justify-between items-center gap-7">
+          <div className="flex w-50 flex-row gap-4">
+            <Input value={searchText} onChange={handleChange} placeholder="Search country" />
+            <Button onClick={searchCountry} disabled={loading}> {loading ? "loading" : 'Submit'} </Button>
+          </div>
+          <div>
+            <DropdownMenu>
+              <DropdownMenuTrigger>{filter ? filter : 'Select'}</DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56">
+                <DropdownMenuItem onClick={() => {
+                  setFilterData(data);
+                  setFilter('')
+                }}> Select </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuRadioGroup onValueChange={updateFilter}>
+                  {stateProvidance?.map((item: string, index: number) => {
+                    if (item) {
+                      return (<DropdownMenuRadioItem key={index} value={item}>{item}</DropdownMenuRadioItem>)
+                    }
+                  }
+                  )}
+                </DropdownMenuRadioGroup>
+              </DropdownMenuContent>
+
+            </DropdownMenu>
+          </div>
+
+
+        </div>
+        <div className="flex flex-row  items-center flex-wrap justify-evenly w-[70%]">
+          {filterData?.map((item: any, index: number) => {
+            return <CollegeCard name={item.name} webPages={item.web_pages} key={index} />
+          })}
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
---
+++ src/components/ui/CollegeCard.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { Button } from "./button";
+import { Card, CardContent, CardFooter, CardHeader } from "./card"
+import { usePDF } from "react-to-pdf";
+
+type CollegeCardProps = {
+  name: string,
+  webPages: string[] | null;
+}
+
+const CollegeCard = (props: CollegeCardProps) => {
+  const { toPDF, targetRef } = usePDF({ filename: 'page.pdf' });
+
+  return (
+    <div className="w-[50%]">
+      <Card className="m-4" ref={targetRef}>
+        <div>
+          <CardHeader>
+            {props.name}
+          </CardHeader>
+          <CardContent>
+            <Link href={props.webPages?.length && props.webPages[0]} className="text-gray-700 underline"> College Link </Link>
+          </CardContent>
+        </div>
+        <CardFooter>
+          <Button onClick={() => toPDF()}> Download </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  )
+}
+export default CollegeCard
+
